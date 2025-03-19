1ab6be424455a7c794c99498b050385d976559d4
Gustavo Moisés Tartas <guga.m@hotmail.com>
2025-03-19 17:42:10 -0300

    override

--- java_curso/Sandero.java
+++ java_curso/Sandero.java
@@ -1,4 +1,17 @@
 package java_curso;
 
 public class Sandero implements Carro {
+    @Override
+    public void acelerar() {
+
+    }
+
+    @Override
+    public void frear() {
+
+    }
+    @Override
+    public void parar() {
+
+    }
 }
